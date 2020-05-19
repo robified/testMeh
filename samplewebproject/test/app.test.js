101c862976fc22841523b84015746925588257e4
Robin <daorob@gmail.com>
2020-05-18 22:56:19 -0500

    Implement the final test case for if that's an invalid email input. All test cases passing successfully.

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -20,5 +20,21 @@ it('shows a success message with a valid email', async () => {
 
     const h1 = dom.window.document.querySelector('h1');
 
-    console.log('Contents of h1', h1.innerHTML);
+    // console.log('Contents of h1', h1.innerHTML);
+    assert.strictEqual(h1.innerHTML, 'Looks fabulous!');
+});
+
+it('shows a fail message with an invalid email', async () => {
+    const dom = await render('index.html');
+
+    const input = dom.window.document.querySelector('input');
+    input.value = 'asdfasdf';
+
+    dom.window.document
+        .querySelector('form')
+        .dispatchEvent(new dom.window.Event('submit'));
+
+    const h1 = dom.window.document.querySelector('h1');
+
+    assert.strictEqual(h1.innerHTML, 'Invalid email');
 });
