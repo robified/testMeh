1ce6d53d7652decb8498c0ba1b8807a8cf35863f
Robin <daorob@gmail.com>
2020-05-18 22:08:08 -0500

    Discover a small test issue regarding the contents of an h1.

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -7,3 +7,18 @@ it('has a text input', async () => {
 
     assert(input);
 });
+
+it('shows a success message with a valid email', async () => {
+    const dom = await render('index.html');
+
+    const input = dom.window.document.querySelector('input');
+    input.value = 'asdf@asdf.com';
+
+    dom.window.document
+        .querySelector('form')
+        .dispatchEvent(new dom.window.Event('submit'));
+
+    const h1 = dom.window.document.querySelector('h1');
+
+    console.log('Contents of h1', h1.innerHTML);
+});
