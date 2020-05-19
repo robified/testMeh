2f28c0e3ed96e05b5f75cba0946bcc3da523f3e4
Robin <daorob@gmail.com>
2020-05-18 21:00:52 -0500

    Install chalk package, and add colors to output messages. v2.0.

--- runner.js
+++ runner.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
 
 class Runner {
     constructor() {
@@ -8,6 +9,7 @@ class Runner {
 
     async runTests() {
         for (let file of this.testFiles) {
+            console.log(chalk.gray(`----- ${file.shortName}`));
             const beforeEaches = [];
             global.beforeEach = (fn) => {
                 beforeEaches.push(fn);
@@ -18,17 +20,16 @@ class Runner {
                 beforeEaches.forEach((func) => func());
                 try {
                     fn();
-                    console.log(`OK - ${description}`);
+                    console.log(chalk.green(`OK - ${description}`));
                 } catch (error) {
-                    console.log(`X - ${description}`);
-                    console.log('\t', error.message);
+                    console.log(chalk.red(`X - ${description}`));
+                    console.log(chalk.red('\t', error.message));
                 }
             };
             try {
                 require(file.name);
             } catch (error) {
-                console.log('X - Error Loading File', file.name);
-                console.log(error);
+                console.log(chalk.red(error));
             }
         }
     }
@@ -41,7 +42,7 @@ class Runner {
             const stats = await fs.promises.lstat(filepath);
 
             if (stats.isFile() && file.includes('.test.js')) {
-                this.testFiles.push({ name: filepath });
+                this.testFiles.push({ name: filepath, shortName: file });
             } else if (stats.isDirectory()) {
                 const childFiles = await fs.promises.readdir(filepath);
 
